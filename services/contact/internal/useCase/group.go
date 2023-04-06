fc7613507b66bb8edfeae79557c62ef9c4fc1fd9
Olzhas <olzhas.ilyassov@alg-in.kz>
2023-04-06 11:21:42 +0600

    add small fix

--- services/contact/internal/useCase/group.go
+++ services/contact/internal/useCase/group.go
@@ -2,6 +2,7 @@ package useCase
 
 import (
 	"ol-ilyassov/clean_arch/pkg/type/queryParameter"
+	"ol-ilyassov/clean_arch/services/contact/internal/domain/contact"
 	"ol-ilyassov/clean_arch/services/contact/internal/domain/group"
 
 	"github.com/google/uuid"
@@ -20,4 +21,10 @@ type GroupReader interface {
 	List(parameter queryParameter.QueryParameter) ([]*group.Group, error)
 	ReadByID(ID uuid.UUID) (*group.Group, error)
 	Count( /*Тут можно передавать фильтр*/ ) (uint64, error)
+}
+
+type ContactInGroup interface {
+	CreateContactIntoGroup(groupID uuid.UUID, contacts ...*contact.Contact) ([]*contact.Contact, error)
+	AddContactToGroup(groupID, contactID uuid.UUID) error
+	DeleteContactFromGroup(groupID, contactID uuid.UUID) error
 }
